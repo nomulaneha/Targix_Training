394c6fea446405e4bc2c437fb9cbc76415aa910a
nehanomula <nehanomula123@gmail.com>
2025-10-24 01:39:38 +0530

    recipe reveal slight changes

--- Recipe Reveal Website(Assignment-1)/script.js
+++ Recipe Reveal Website(Assignment-1)/script.js
@@ -10,7 +10,6 @@ const fetchRecipes = async (query)=>{
     try {
     const data = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${query}`);
     const response = await data.json();
-
     recipeContainer.innerHTML = "";
     response.meals.forEach(meal=>{
         const recipeDiv = document.createElement('div');
